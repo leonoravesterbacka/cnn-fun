1de29893ece3a608f3f163e4da84f960ed9fbb01
Leonora Vesterbacka <leonoravesterbacka@gmail.com>
2020-11-03 15:05:54 +0100

    changing structure of cnn dir

--- cnn/ml/plotting.py
+++
@@ -1,7 +0,0 @@
-import numpy as np 
-import matplotlib.pyplot as plt
-
-def imshowPytorch(img):
-    npimg = img.numpy()
-    plt.imshow(np.transpose(npimg, (1, 2, 0)))
-    plt.savefig('images/test.png')
